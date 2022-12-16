4b29acaad02903e5174655b8984b086fba0e69b9
cookedbyishaq <ishaq.hashmi1994@gmail.com>
2022-12-16 22:47:14 +0530

    aborted

--- abortcontroller.js
+++ abortcontroller.js
@@ -3,5 +3,29 @@ const url = "flame.avi"
 
 
 const downloadButton = document.createElement("button")
+downloadButton.textContent = "download"
 
 document.body.appendChild(downloadButton)
+
+downloadButton.addEventListener("click",handle)
+
+function handle(){
+
+    if(controller){
+        console.log("abort")
+        controller.abort()
+        controller =""
+        downloadButton.textContent = "download"
+
+        return
+    } else {
+        controller = new AbortController();
+        const signal = controller.signal
+        downloadButton.textContent ="cancel"
+        fetch(url,{signal}).then((res)=>{
+            console.log("Download finished",res)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+}
